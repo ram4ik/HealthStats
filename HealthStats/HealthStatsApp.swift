e52fc010b2d1fa95e99ee79976b737f8b9359e89
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-07-27 19:48:55 +0300

    Initial Commit

---
+++ HealthStats/HealthStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthStatsApp.swift
+//  HealthStats
+//
+//  Created by Ramill Ibragimov on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HealthStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
