97f5109f7ccba31c7830f849482f1e496cd53a7b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-07-29 11:55:41 +0300

    Grid Tiles UI

--- HealthStats/Views/ContentView.swift
+++ HealthStats/Views/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var repository = HKRepository()
+    
+    var items: [GridItem] {
+        Array(repeating: .init(.adaptive(minimum: 120)), count: 2)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: items, spacing: 2) {
+                    ForEach(Activity.allActivities()) { activity in
+                        NavigationLink(destination: Text(activity.name)) {
+                            VStack {
+                                Text(activity.image)
+                                    .frame(width: 50, height: 50)
+                                    .background(RoundedRectangle(cornerRadius: 5).fill(Color.blue.opacity(0.2)))
+                                
+                                Text(activity.name)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue.opacity(0.2)))
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }.padding()
+            }.navigationTitle("My health stats")
+        }.onAppear() {
+            repository.requestAuthorization { success in
+                print("Auth success? \(success)")
+            }
+        }
     }
 }
 
